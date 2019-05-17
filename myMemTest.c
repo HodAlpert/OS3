0961766b1e9c8743a5d8c45378c342bd3a77ef46
Shoham Peller <shohamp@gmail.com>
2019-05-17 12:52:55 +0300

    Added test for new pmalloc/pfree functions

--- myMemTest.c
+++ myMemTest.c
@@ -1,7 +1,13 @@
+#include "param.h"
 #include "types.h"
 #include "user.h"
 
 int main() {
+  void* new_page = pmalloc();
+  memset(new_page, 2, PGSIZE);
+  protect_page(new_page);
+  memset(new_page, 2, PGSIZE); // Should fail
+  pfree(new_page);
   printf(1, "Test PASSED!\n");
   exit();
 }
