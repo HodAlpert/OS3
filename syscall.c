95fa90cc6a96bf8ea5b4e06d4a59268c5dd969ae
Hod Alpert <hod.alpert@stratoscalse.com>
2019-05-21 08:35:39 +0300

    implemented light the bit method

--- syscall.c
+++ syscall.c
@@ -104,6 +104,8 @@ extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
 extern int sys_yield(void);
+extern int sys_light_pmalloc_bit(void);
+
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -128,6 +130,7 @@ static int (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_yield]   sys_yield,
+[SYS_light_pmalloc_bit] sys_light_pmalloc_bit,
 };
 
 void
